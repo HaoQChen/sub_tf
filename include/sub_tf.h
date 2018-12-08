9eff09c995246af9d82ede1dd8b5901420fb7c34
HaoQChen <1218138462@qq.com>
2018-12-08 21:14:56 +0800

    add file save

--- include/sub_tf.h
+++ include/sub_tf.h
@@ -5,17 +5,27 @@
 #include "tf/transform_listener.h"
 #include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
+#include <fstream>
 
 #include <string>
 
 class FrameTransform{
 public:
-	FrameTransform(std::string _tarf, std::string _sourf):target_frame_(_tarf), source_frame_(_sourf){}
-	geometry_msgs::PoseStamped getTransform();
+	FrameTransform(std::string _tarf, std::string _sourf, const char* filename):target_frame_(_tarf), source_frame_(_sourf){
+		save_file_.open(filename, std::ios::out);
+		if(!save_file_){
+			ROS_ERROR("Can not create the file!");
+		}
+	}
+	~FrameTransform(){
+		save_file_.close();
+	}
+	bool getTransform(geometry_msgs::PoseStamped& ps);
 private:
 	tf::TransformListener listener_;
 	std::string target_frame_;
 	std::string source_frame_;
+	std::ofstream save_file_;
 };
 
 #endif
--- src/sub_tf.cc
+++ src/sub_tf.cc
@@ -9,16 +9,16 @@
 
 #include "sub_tf.h"
 
-geometry_msgs::PoseStamped FrameTransform::getTransform()
+bool FrameTransform::getTransform(geometry_msgs::PoseStamped& ps)
 {   
     tf::StampedTransform transform;
-    geometry_msgs::PoseStamped ps;
 
     listener_.waitForTransform(target_frame_, source_frame_, ros::Time(0), ros::Duration(0.2));
     try{
         listener_.lookupTransform(target_frame_, source_frame_, ros::Time(0), transform);
     }catch(tf::TransformException e){
         ROS_ERROR("%s", e.what());
+        return false;
     }
 
     ps.header.stamp = transform.stamp_;
@@ -35,8 +35,13 @@ geometry_msgs::PoseStamped FrameTransform::getTransform()
     double yaw, pitch, roll;
     transform.getBasis().getEulerYPR(yaw, pitch, roll);
     ROS_INFO("YPR: (%f, %f, %f)", yaw, pitch, roll);
-
-    return ps;
+    if(!save_file_){
+        ROS_ERROR("Can not create the file!");
+    }else{
+        save_file_<<ps.header.stamp<<","<<ps.pose.position.x<<","<<ps.pose.position.y<<","\
+              <<ps.pose.position.z<<","<<yaw<<","<<pitch<<","<<roll<<std::endl;
+    } 
+    return true;
 }
 
 int main(int argc, char** argv)
@@ -44,12 +49,15 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "sub_tf");
     ros::NodeHandle nh;
     ros::Rate loop(20);
-    FrameTransform base2map("/map", "base_link");
+    FrameTransform base2map("/map", "base_link", "/home/scut214/Documents/pre_base2map.txt");
     ros::Publisher pub_tf_b2m = nh.advertise<geometry_msgs::PoseStamped>("tf_b2m", 1, true);
     
     while(ros::ok()){
-        geometry_msgs::PoseStamped ps_base2map = base2map.getTransform();
-        pub_tf_b2m.publish(ps_base2map);
+        geometry_msgs::PoseStamped ps_base2map;
+        bool is_transform = base2map.getTransform(ps_base2map);
+        if(is_transform){
+            pub_tf_b2m.publish(ps_base2map);
+        }
         loop.sleep();
     }
     return 0;
